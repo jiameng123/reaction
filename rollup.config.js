b6e1127a4c34bbb01ae6f9b04a6bb6505d866be3
贾蒙 <386805508@qq.com>
2020-06-28 23:31:55 +0800

    feat: init

--- rollup.config.js
+++ rollup.config.js
@@ -2,17 +2,48 @@ import typescript from '@rollup/plugin-typescript';
 import commonjs from '@rollup/plugin-commonjs';
 import resolve from '@rollup/plugin-node-resolve';
 import babel from '@rollup/plugin-babel';
+import path from "path";
 
 export default {
   input: "src/index.ts",
-  output: {
-    dir: 'dist',
-    format: 'cjs'
-  },
+  output:[ 
+    {
+      file:"./dist/index.cjs.js",
+      format: 'cjs'
+    },
+    {
+     
+      file:"./dist/index.amd.js",
+      format: 'amd'
+    },
+    {
+     
+      file:"./dist/index.esm.js",
+      format: 'esm'
+    },
+     {
+     
+      file:"./dist/index.iife.js",
+      format: 'iife'
+    },
+    {
+     
+      file:"./dist/index.umd.js",
+      format: 'umd'
+    },
+    {
+      file:"./dist/index.system.js",
+      format: 'system'
+    }
+  ],
   plugins: [
     resolve(),
     commonjs(),
     typescript(),
-    babel()
+    babel({
+      configFile: path.resolve(__dirname, '.babelrc.json'),
+      allowAllFormats: true,
+      babelHelpers:'runtime'
+    })
   ]
 }
---
+++ src/index.ts
@@ -0,0 +1,8 @@
+const a:string = "b";
+
+
+export default class BB {
+    getA() {
+        console.log(1)
+    }
+}
